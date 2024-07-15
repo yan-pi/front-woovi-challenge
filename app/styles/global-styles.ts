435c1cdea4e49ea923de1ff59a29865af477ac78
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-07-15 17:34:59 -0300

    feat: add mui and styled components config

---
+++ app/styles/global-styles.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Nunito", sans-serif;
+  font-size: 16px;
+}
+
+b {
+  font-weight: 800;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+ul, ol {
+    list-style: none;
+}
+
+h1, h2, h3, h4, h5,h6, a, ul, li, span, p {
+  color: ${(props) => props.theme.colors.primary};
+}
+
+h1 {
+  font-size: ${(props) => props.theme.fontSizes.xl};
+}
+`;
---
+++ app/utils/theme.d.ts
@@ -0,0 +1,42 @@
+// src/theme.d.ts
+import { Theme } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    customColors: {
+      thirdiary: string;
+      tag: string;
+      selected: string;
+      checked: string;
+      label: string;
+    };
+    customBorders: {
+      primary: string;
+      thirdiary: string;
+    };
+    customBackgrounds: {
+      buttonPrimary: string;
+      buttonHover: string;
+      buttonDisabled: string;
+    };
+  }
+
+  interface ThemeOptions {
+    customColors?: {
+      thirdiary?: string;
+      tag?: string;
+      selected?: string;
+      checked?: string;
+      label?: string;
+    };
+    customBorders?: {
+      primary?: string;
+      thirdiary?: string;
+    };
+    customBackgrounds?: {
+      buttonPrimary?: string;
+      buttonHover?: string;
+      buttonDisabled?: string;
+    };
+  }
+}
